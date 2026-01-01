d3fab32528a17e3b734b5dd777291c6477371f66
unknown <rishavdas27602@gmail.com>
2026-01-01 21:30:57 +0530

    first commit of 2026

---
+++ Plus_One_66.java
@@ -0,0 +1,15 @@
+class Plus_One_66{
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        for(int i = l - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int res[] = new int[l + 1];
+        res[0] = 1;
+        return res;
+    }
+}

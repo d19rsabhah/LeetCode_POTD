a79ba2f4d4ccdce4861ec3fe6cbcc06859b8cc20
unknown <rishavdas27602@gmail.com>
2026-02-03 21:51:01 +0530

    31st POTD solved on LeetCode

---
+++ Find_Smallest_Letter_Greater_Than_Target_744.java
@@ -0,0 +1,8 @@
+class Find_Smallest_Letter_Greater_Than_Target_744{
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (char c : letters) {
+            if (c > target) return c;
+        }
+        return letters[0];
+    }
+}

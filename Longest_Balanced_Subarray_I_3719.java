88d182d4ede52dfe16db6ac0e93a2193f4c6e72d
unknown <rishavdas27602@gmail.com>
2026-02-11 01:00:37 +0530

    41st POTD solved

---
+++ Longest_Balanced_Subarray_I_3719.java
@@ -0,0 +1,23 @@
+class Longest_Balanced_Subarray_I_3719 {
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            HashSet<Integer>odd = new HashSet<>();
+            HashSet<Integer>even = new HashSet<>();
+            for(int j = i; j < n; j++){
+                if(nums[j] % 2 == 0){
+                    even.add(nums[j]);
+                }else{
+                    odd.add(nums[j]);
+                }
+                if(odd.size() == even.size()){
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+            
+        }
+        return ans;
+        
+    }
+}

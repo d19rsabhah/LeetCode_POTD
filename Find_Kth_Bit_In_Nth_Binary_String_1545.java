7d7c0111e50ba616eb5dbf21dcd50c115dff309e
unknown <rishavdas27602@gmail.com>
2026-03-03 23:09:54 +0530

    62nd POTD solved

---
+++ Find_Kth_Bit_In_Nth_Binary_String_1545.java
@@ -0,0 +1,19 @@
+class Find_Kth_Bit_In_Nth_Binary_String_1545 {
+    public char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+        
+        int length = (1 << n) - 1;       // 2^n - 1
+        int mid = (length + 1) / 2;      // 2^(n-1)
+        
+        if (k == mid) {
+            return '1';
+        } 
+        else if (k < mid) {
+            return findKthBit(n - 1, k);
+        } 
+        else {
+            char ch = findKthBit(n - 1, length - k + 1);
+            return ch == '0' ? '1' : '0';   // invert
+        }
+    }
+}

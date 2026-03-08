547e5d008c07a47d99aa880f15953fe47701e397
unknown <rishavdas27602@gmail.com>
2026-03-08 23:51:01 +0530

    67th POTD SOLVED

---
+++ Find_Unique_Binary_String_1980.java
@@ -0,0 +1,10 @@
+class Find_Unique_Binary_String_1980 {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
+        }
+        return sb.toString();
+    }
+}
